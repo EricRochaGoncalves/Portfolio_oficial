1c34f57235ca79f9ead0022ea171c0b644d18a96
Eric Gonçalves <166281170+EricRochaGoncalves@users.noreply.github.com>
2024-11-02 17:39:28 -0300

    Atualizar o script.js

--- script.js
+++ script.js
@@ -22,20 +22,4 @@ function toggleBodyOverflow(isActive) {
     document.body.style.overflow = isActive ? 'hidden' : '';
 }
 
-function animateMenu(isActive) {
-    const nav = document.getElementById('nav');
-    if (isActive) {
-        nav.style.opacity = '0';
-        nav.style.transform = 'translateY(-20px)';
-        requestAnimationFrame(() => {
-            nav.style.transition = 'opacity 0.3s ease, transform 0.3s ease';
-            nav.style.opacity = '1';
-            nav.style.transform = 'translateY(0)';
-        });
-    } else {
-        nav.style.transition = 'opacity 0.3s ease, transform 0.3s ease';
-        nav.style.opacity = '0';
-        nav.style.transform = 'translateY(-20px)';
-    }
-}
 // Pra que tantos códigos, sendo que a vida não é programada, e as melhores coisas não têm lógica?
