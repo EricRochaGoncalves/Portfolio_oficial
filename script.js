9cbf0a49ffdbab9b00a233b29b05bf3e36d4a556
Eric Gonçalves <166281170+EricRochaGoncalves@users.noreply.github.com>
2025-02-26 13:11:45 -0300

    Atualizar o script.js

--- script.js
+++ script.js
@@ -33,3 +33,22 @@ document.getElementById('cardOverlay').addEventListener('click', function(event)
         }, 500); // Tempo igual ao da animação
     }
 });
+
+// Seleciona todos os links de navegação
+const navLinks = document.querySelectorAll('nav a');
+
+// Função de rolagem suave
+navLinks.forEach(link => {
+    link.addEventListener('click', function(e) {
+        e.preventDefault(); // Previne o comportamento padrão do link
+
+        // Obtém o ID do link (exemplo: "#home")
+        const targetSection = document.querySelector(this.getAttribute('href'));
+
+        // Rolagem suave até a seção correspondente
+        targetSection.scrollIntoView({
+            behavior: 'smooth'
+        });
+    });
+});
+
